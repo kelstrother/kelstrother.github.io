b6b819f1b65e9484e9cd5d99f2f5b5039f713385
Kel Strother <kelstrother@gmail.com>
2020-12-30 00:49:02 -0700

    skills page, contact form, header

--- js/app.js
+++ js/app.js
@@ -26,95 +26,18 @@ $.ajax('https://spreadsheets.google.com/feeds/list/12hAZRGTB9zMFiXmOJCIsyhI1Fo6s
     ///////////////////////////////////////////////////
 for (i = 0; i < projects.length; i++) {
   const $div = $(
-                 `<sl-card class="card-overview">
-                  <img slot="image" src=${projects[i].img} 
-                  alt="Portfolio Project"><strong><h3 class=projname>${projects[i].name}</h3></strong>
-                  <p class=description>${projects[i].description}</p>
-                  <div-slot="footer">
-                  <sl-button class='git' href=${projects[i].git}>The Code</sl-button>
-                  <sl-button class='live' href=${projects[i].live}>The Project</sl-button>
-                  </sl-card>`
+                `<div class="card">
+                  <div class="card-section">
+                    <img src=${projects[i].img} alt="Portfolio Work">
+                  </div>
+                  <div class="card-section">
+                    <strong><p class="text-center">${projects[i].name}</strong><br>
+                    ${projects[i].description}<br><br>
+                    <a href=${projects[i].git} class="button" id="codebtn">The Code</a>
+                    <a href=${projects[i].live} class="button" id="projectbtn">The Project</a></p>
+                  </div>
+                 </div>`
    )
  $('#creations').append($div)  
   }
 })
-
-
-
-  
-  
-  
-  
-  
-  
-
-  
- 
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////
-//                       PULL DATA FROM BLOG FROM HEADLESS CMS
-/////////////////////////////////////////////////
-
-
-
-
-
-
-
-/////////////////////////////////////////////////
-//                       CONTACT FORM
-/////////////////////////////////////////////////
-
-//   window.addEventListener("DOMContentLoaded", function() {
-
-//     // get the form elements defined in your form HTML above
-    
-//     var form = document.getElementById("fs-frm");
-//     var button = document.getElementById("fs-frm-inputs");
-//     // var status = document.getElementById("my-form-status");
-
-//     // Success and Error functions for after the form is submitted
-    
-//  function success() {
-//       form.reset();
-//       button.style = "display: none ";
-//       status.innerHTML = "Thanks!";
-//     }
-
-//     function error() {
-//       status.innerHTML = "Oops! There was a problem.";
-//     }
-
-//     // handle the form submission event
-
-//     form.addEventListener("submit", function(ev) {
-//       ev.preventDefault();
-//       var data = new FormData(form);
-//       ajax(form.method, form.action, data, success, error);
-//     });
-//   });
-  
-//   // helper function for sending an AJAX request
-
-//   function ajax(method, url, data, success, error) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open(method, url);
-//     xhr.setRequestHeader("Accept", "application/json");
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState !== XMLHttpRequest.DONE) return;
-//       if (xhr.status === 200) {
-//         success(xhr.response, xhr.responseType);
-//       } else {
-//         error(xhr.status, xhr.response, xhr.responseType);
-//       }
-//     };
-//     xhr.send(data);
-  
