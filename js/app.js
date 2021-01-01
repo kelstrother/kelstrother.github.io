095e9e3b4502a52e1455c366496642eb9a4e1f98
Kel Strother <kelstrother@gmail.com>
2021-01-01 12:37:16 -0700

    finished up mobile, started on tablet

--- js/app.js
+++ js/app.js
@@ -26,17 +26,29 @@ $.ajax('https://spreadsheets.google.com/feeds/list/12hAZRGTB9zMFiXmOJCIsyhI1Fo6s
     ///////////////////////////////////////////////////
 for (i = 0; i < projects.length; i++) {
   const $div = $(
-                `<div class="card">
-                  <div class="card-section">
-                    <img src=${projects[i].img} alt="Portfolio Work">
+                  `<div class="card">
+                  <div class="cardimg">
+                    <img src=${projects[i].img} alt="Project Example" class="project">
                   </div>
-                  <div class="card-section">
-                    <strong><p class="text-center">${projects[i].name}</strong><br>
-                    ${projects[i].description}<br><br>
-                    <a href=${projects[i].git} class="button" id="codebtn">The Code</a>
-                    <a href=${projects[i].live} class="button" id="projectbtn">The Project</a></p>
+                  <div class="cardtext">
+                    <p class="description"><strong>${projects[i].name}</strong><br>${projects[i].description}</p>
                   </div>
-                 </div>`
+                  <div class="button">
+                    <a href=${projects[i].git} class="btncode"><button>The Code</button></a>
+                    <a href=${projects[i].live} class="btnproject"><button>The Project</button></a>
+                  </div>
+                </div>`
+                // `<div class="card">
+                //   <div class="card-section">
+                //     <img src=${projects[i].img} alt="Portfolio Work">
+                //   </div>
+                //   <div class="card-section">
+                //     <strong><p class="text-center">${projects[i].name}</strong><br>
+                //     ${projects[i].description}<br><br>
+                //     <a href=${projects[i].git} class="button" id="codebtn">The Code</a>
+                //     <a href=${projects[i].live} class="button" id="projectbtn">The Project</a></p>
+                //   </div>
+                //  </div>`
    )
  $('#creations').append($div)  
   }
